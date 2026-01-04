540991b9468f58c20ffc82312dd665e5e2437b5e
Kevin Yokley <kevin.yokley@oracle.com>
2026-01-04 11:23:34 -0600

    Apply formatting

--- flake.nix
+++ flake.nix
@@ -6,96 +6,110 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs =
-    inputs@{ self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
 
-      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+      imports = [inputs.flake-parts.flakeModules.easyOverlay];
 
-      perSystem =
-        {
-          self',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          _module.args.pkgs = import self.inputs.nixpkgs {
-            inherit system;
-            overlays = [ inputs.self.overlays.default ];
-            config = {
-              allowUnfree = true;
-            };
-          };
-
-          formatter = pkgs.nixfmt-rfc-style;
-
-          packages.oracle-database = pkgs.callPackage ./packages/oracle-database.nix { };
-          packages.oracle-database-test = pkgs.testers.runNixOSTest ./tests/integration/oracle-database.nix;
-          packages.oracle-database-container-test = pkgs.testers.runNixOSTest ./tests/integration/oracle-database-container.nix;
-
-          checks = {
-            # oracle-database = pkgs.testers.runNixOSTest ./tests/integration/oracle-database.nix;
-            # oracle-database-container = pkgs.testers.runNixOSTest ./tests/integration/oracle-database-container.nix;
-
-            containerModuleTest = pkgs.testers.runNixOSTest {
-              name = "containerModuleTest";
-              nodes = {
-                db = {
-                  imports = [
-                    ./modules/oracle-database-container.nix
-                  ];
-
-                  services.oracle-database-container = {
-                    enable = true;
-                    passwordFile = toString (builtins.toFile "password.txt" ''
-                    password
-                    '');
-                    # Explicitly use the package from the nix-oracle-db flake,
-                    # avoiding reliance on pkgs having an overlay.
-                    # package = self'.packages.oracle-database;
-                    openFirewall = true;
-                    initScriptDir = null;
-                  };
-                };
-              };
-              testScript = ''
-                start_all()
-              '';
-            };
-
-            # moduleTest = pkgs.testers.runNixOSTest {
-            #   name = "moduleTest";
-            #   nodes = {
-            #     db = {
-            #       imports = [
-            #         ./modules/oracle-database.nix
-            #       ];
-            #       environment.systemPackages = [
-            #         pkgs.vim
-            #       ];
-
-            #       services.oracle-database = {
-            #         enable = true;
-            #         passwordFile = ./password.txt;
-            #         # Explicitly use the package from the nix-oracle-db flake,
-            #         # avoiding reliance on pkgs having an overlay.
-            #         # package = self'.packages.oracle-database;
-            #         openFirewall = true;
-            #       };
-            #     };
-            #   };
-            #   testScript = ''
-            #     start_all()
-            #   '';
-            # };
-          };
-
-          overlayAttrs = {
-            oracle-database = self'.packages.oracle-database;
+      perSystem = {
+        self',
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import self.inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.self.overlays.default];
+          config = {
+            allowUnfree = true;
           };
         };
+
+        formatter = pkgs.nixfmt-rfc-style;
+
+        packages.oracle-database = pkgs.callPackage ./packages/oracle-database.nix {};
+        packages.oracle-database-test = pkgs.testers.runNixOSTest ./tests/integration/oracle-database.nix;
+        packages.oracle-database-container-test = pkgs.testers.runNixOSTest ./tests/integration/oracle-database-container.nix;
+
+        checks = {
+          # oracle-database = pkgs.testers.runNixOSTest ./tests/integration/oracle-database.nix;
+          # oracle-database-container = pkgs.testers.runNixOSTest ./tests/integration/oracle-database-container.nix;
+
+          containerModuleTest = pkgs.testers.runNixOSTest {
+            name = "containerModuleTest";
+            nodes = {
+              db = {
+                imports = [
+                  ./modules/oracle-database-container.nix
+                ];
+
+                services.oracle-database-container = {
+                  enable = true;
+                  passwordFile = toString (builtins.toFile "password.txt" ''
+                    password
+                  '');
+                  # Explicitly use the package from the nix-oracle-db flake,
+                  # avoiding reliance on pkgs having an overlay.
+                  # package = self'.packages.oracle-database;
+                  openFirewall = true;
+                  initScript = builtins.toFile "01_create.sql" ''
+                    ALTER SESSION SET CONTAINER=FREEPDB1;
+                    CREATE USER TEST IDENTIFIED BY test QUOTA UNLIMITED ON USERS;
+                    GRANT CONNECT, RESOURCE TO TEST;
+
+                    CREATE TABLE student (
+                        last_name       VARCHAR2(15) NOT NULL,
+                        first_name      VARCHAR2(15) NOT NULL,
+                        id              NUMBER(6) PRIMARY KEY
+                    );
+                    INSERT INTO students (last_name, first_name, id)
+                    VALUES ('Doe', 'John', 1001);
+
+                  '';
+                };
+              };
+            };
+            testScript = ''
+              start_all()
+            '';
+          };
+
+          # moduleTest = pkgs.testers.runNixOSTest {
+          #   name = "moduleTest";
+          #   nodes = {
+          #     db = {
+          #       imports = [
+          #         ./modules/oracle-database.nix
+          #       ];
+          #       environment.systemPackages = [
+          #         pkgs.vim
+          #       ];
+
+          #       services.oracle-database = {
+          #         enable = true;
+          #         passwordFile = ./password.txt;
+          #         # Explicitly use the package from the nix-oracle-db flake,
+          #         # avoiding reliance on pkgs having an overlay.
+          #         # package = self'.packages.oracle-database;
+          #         openFirewall = true;
+          #       };
+          #     };
+          #   };
+          #   testScript = ''
+          #     start_all()
+          #   '';
+          # };
+        };
+
+        overlayAttrs = {
+          oracle-database = self'.packages.oracle-database;
+        };
+      };
 
       flake = {
         nixosModules.oracle-database = import ./modules/oracle-database.nix;
